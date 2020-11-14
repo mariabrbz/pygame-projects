9121f3fa00c7126bcfce84d447bcaa7ebd95466a
mariabrbz <39411025+mariabrbz@users.noreply.github.com>
2020-11-14 21:41:00 +0100

    matching tiles 1

---
+++ matching-tiles/animal.py
@@ -0,0 +1,30 @@
+import os
+import random
+import game_config as gc
+
+from pygame import image, transform
+
+animals_count = dict([(a, 0) for a in gc.ASSET_FILES])
+
+def available_animals():
+    res = []
+    for a, c in animals_count.items():
+        if c < 2:
+           res.append(a)     
+    return res
+
+class Animal:
+    def __init__(self, index):
+        self.index = index
+        self.row = index // gc.NUM_TILES_SIDE
+        self.col = index % gc.NUM_TILES_SIDE
+        self.name = random.choice(available_animals())
+        animals_count[self.name] += 1
+
+        self.front_path = os.path.join(gc.ASSET_DIR, self.name)
+        self.image = image.load(self.front_path)
+        self.image = transform.scale(self.image, (gc.IMAGE_SIZE - 2 * gc.MARGIN, gc.IMAGE_SIZE - 2 * gc.MARGIN))
+        #self.box = image.load()
+        #self.box = self.image.copy()
+        #self.box.fill((238, 130, 238))
+        self.skip = False
